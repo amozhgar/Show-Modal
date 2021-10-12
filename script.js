eefbaab9c94e0da7267f5e21955f8d10fab9c21f
amozhgar <75085142+amozhgar@users.noreply.github.com>
2021-10-12 18:55:50 +0300

    Update script.js

--- script.js
+++ script.js
@@ -18,3 +18,8 @@ const closeModal = () => {
 };
 btn.addEventListener("click", closeModal);
 overlay.addEventListener("click", closeModal);
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+    closeModal();
+  }
+});
